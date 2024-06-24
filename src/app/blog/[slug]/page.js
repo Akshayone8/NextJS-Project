5c244a66b8cd545f1a3f85d5b1ced758122be8c0
Akshay <62467248+Akshayone8@users.noreply.github.com>
2024-06-24 20:01:53 +0530

    added code of about and blog page

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="" alt="single blog image" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image src="" alt="single Image" fill className={styles.avatar} />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Date </span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.24</span>
+          </div>
+          <div className={styles.content}>Desc</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          fill
+          alt="contact image"
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="FullName" />
+          <input type="email" placeholder="Email Address" />
+          <input type="number" placeholder="Phone Number (Optional)" />
+          <textarea placeholder="Message" cols={30} rows={10}></textarea>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
